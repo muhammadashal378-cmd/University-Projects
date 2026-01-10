abb91a6822ff87d1864a8a00bf7ae8ee10a04ce1
Muhammad Ashal <muhammadashal378@gmail.com>
2026-01-10 16:07:27 +0500

    Initial Basics Java program: Add two numbers and dataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int integerVar = 100;
+        double doubleVar = 99.99;
+        char charVar = 'A';
+        boolean boolVar = true;
+        String stringVar = "Hello, World!";
+        
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("Boolean: " + boolVar);
+        System.out.println("String: " + stringVar);
+    }
+}
