f183bcab49e78d34259ce359f1c80895bcefa606
Muhammad Ashal <muhammadashal378@gmail.com>
2026-01-11 15:32:15 +0500

    message

---
+++ src/powerclaculation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class powerclaculation{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Value Of Voltage:" + "\n");
+        double V = sc.nextDouble();  
+    
+        System.out.println("Enter The Value Of Current");
+        double A = sc.nextDouble();     
+        
+        double p = V * A;
+
+        System.out.println("The Power Is; " + p + "Watts");
+        sc.close();
+    }
+}
+    
+
