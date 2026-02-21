14106842d2001cba4173d4aaa4dbb21bd34be99a
Muhammad Ashal <muhammadashal378@gmail.com>
2026-02-21 23:22:09 +0500

    message

---
+++ src/table.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+public class table {
+    public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter The Number To Print The Table:");
+    int n = sc.nextInt();
+    int i =1;
+    switch(n){
+        case 1:
+        for(   i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 2:
+        for( i=1; i<=10; i++) 
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 3:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 4:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 5:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 6:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 7:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 8:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 9:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        case 10:
+        for( i=1; i<=10; i++)
+        {
+            System.out.println( n + " x " + i + " = " + n*i);
+        }
+        break;
+        default:
+    }
+    
+}    
+}
