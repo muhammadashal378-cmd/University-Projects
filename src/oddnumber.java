ee9fcefece448d351097a2cf64ebfa503feeb13d
Muhammad Ashal <muhammadashal378@gmail.com>
2026-02-21 22:19:29 +0500

    message

---
+++ src/oddnumber.java
@@ -0,0 +1,13 @@
+public class oddnumber {
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<=50)
+        {
+            if(i%2!=0)
+            System.out.println( i);
+            i = i+2;
+        }
+    }
+    
+}
