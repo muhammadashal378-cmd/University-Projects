2e29912905db8e23b1eb7e458ea8b2048ba079c7
Muhammad Ashal <muhammadashal378@gmail.com>
2026-02-21 22:16:11 +0500

    message

---
+++ src/evennumber.java
@@ -0,0 +1,13 @@
+public class evennumber {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i<=50)
+        {
+            if(i%2==0)
+            System.out.println( i);
+            i = i+2;
+        }
+    }
+    
+}
