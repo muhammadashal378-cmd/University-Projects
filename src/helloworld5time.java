a227524f517a768288a5e247a6f7d56185c914cd
Muhammad Ashal <muhammadashal378@gmail.com>
2026-02-21 22:04:44 +0500

    message

---
+++ src/helloworld5time.java
@@ -0,0 +1,11 @@
+public class helloworld5time {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        for(i=0; i<5; i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+    
+}
