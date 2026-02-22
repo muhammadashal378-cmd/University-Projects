6134c46d716f0b9464169e67b2cf7410396f065e
Muhammad Ashal <muhammadashal378@gmail.com>
2026-02-22 14:43:34 +0500

    message

---
+++ src/mutipleof5skip.java
@@ -0,0 +1,13 @@
+public class mutipleof5skip {
+    public static void main(String[] args)
+    {
+        for(int i = 1; i<=100; )
+        {if(i%5!=0)
+        {
+            System.out.println(i);
+        }
+        i++;
+    }
+    } 
+    
+}
